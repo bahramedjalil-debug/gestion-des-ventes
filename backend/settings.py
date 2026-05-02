2be5e7791ebeaaf50cdeb991d70a33d36baa89ff
it's bahrame <bahramedjalil@gmail.com>
2026-05-02 20:53:31 +0100

    fix postgres configuration

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import dj_database_url
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -91,7 +92,9 @@ REST_FRAMEWORK = {
 
 
 DATABASES = {
-    "default": dj_database_url.parse("YOUR_DATABASE_URL_HERE")
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
 }
 
 
