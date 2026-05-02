acb383354091e0c2b860fe485856f1d51ecc4466
it's bahrame <bahramedjalil@gmail.com>
2026-05-02 20:19:46 +0100

    fix jwt token routing conflict

--- backend/urls.py
+++ backend/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     path('api/', include('api.urls')),
 
     
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
