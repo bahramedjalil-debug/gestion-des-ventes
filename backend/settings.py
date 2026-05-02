e70081ef0bdcbab5f1553dc859f31323705787e2
it's bahrame <bahramedjalil@gmail.com>
2026-05-02 20:11:09 +0100

    fix api urls for production

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5w9%$nu#(5^fubk(%-*m$vh$_#@^kzrfc70ujofvc=joo+^n6z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://gestion-des-ventes-hvcb.onrender.com/"]
 
 
 # Application definition
